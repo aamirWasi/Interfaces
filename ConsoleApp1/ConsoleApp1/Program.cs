6c121583562037bea7e226057bb3871163eac641
aamir <wasi3aamir@gmail.com>
2019-01-31 11:37:42 +0600

    Interfaces basic

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle buick = new Vehicle("Buick", 160, 4);
+            if(buick is IDrivable)
+            {
+                buick.Move();
+                buick.Stop();
+            }
+            else
+            {
+                Console.WriteLine($"The {buick.Brand} can't driven");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vehicle : IDrivable
+    {
+        public string Brand { get; set; }
+        public int Wheels { get; set; }
+        public double Speed { get; set; }
+
+        //Constructor
+        public Vehicle(string brand,
+            double speed,
+            int wheels)
+        {
+            Brand = brand;
+            Speed = speed;
+            Wheels = wheels;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"The {Brand} Moves Forward at {Speed} MPH"); ;
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"The {Brand} stops");
+            Speed = 0;
+        }
+    }
+}
